94c7b6f9d8ed44ba270b73002e6629ceef2e31ee
Szymon Polanowski <szympol@hotmail.com>
2019-11-24 23:10:55 +0100

    Add basic routing

---
+++ src/views/Brazil.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Brazil</h2>
+    <p>Officia aliquam molestiae.</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
