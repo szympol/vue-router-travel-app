ad41b9c654b1f34a07ab4f9141d92e149ac4bb9a
Szymon Polanowski <szympol@hotmail.com>
2019-12-04 11:41:05 +0100

    Add static and dynamic route

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Brazil from "../views/Brazil.vue";
-import Hawaii from "../views/Hawaii.vue";
-import Jamaica from "../views/Jamaica.vue";
-import Panama from "../views/Panama.vue";
+// import Brazil from "../views/Brazil.vue";
+// import Hawaii from "../views/Hawaii.vue";
+// import Jamaica from "../views/Jamaica.vue";
+// import Panama from "../views/Panama.vue";
 
 Vue.use(VueRouter);
 
@@ -26,26 +26,43 @@ const routes = [
   {
     path: "/brazil",
     name: "brazil",
-    component: Brazil
+    // component: Brazil,
+    component: () =>
+      import(/* webpackChunkName: "brazil" */ "../views/Brazil.vue")
   },
   {
     path: "/hawaii",
     name: "hawaii",
-    component: Hawaii
+    // component: Hawaii,
+    component: () =>
+      import(/* webpackChunkName: "hawaii" */ "../views/Hawaii.vue")
   },
   {
     path: "/jamaica",
     name: "jamaica",
-    component: Jamaica
+    // component: Jamaica
+    component: () =>
+      import(/* webpackChunkName: "jamaica" */ "../views/Jamaica.vue")
   },
   {
     path: "/panama",
     name: "panama",
-    component: Panama
+    // component: Panama,
+    component: () =>
+      import(/* webpackChunkName: "panama" */ "../views/Panama.vue")
+  },
+  {
+    path: "/details/:id",
+    name: "DestinationDetails",
+    component: () =>
+      import(
+        /* webpackChunkName: "DestinationDetails" */ "../views/DestinationDetails.vue"
+      )
   }
 ];
 
 const router = new VueRouter({
+  linkExactActiveClass: "vue-school-active-class",
   routes
 });
 
